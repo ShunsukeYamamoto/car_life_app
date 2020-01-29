3190b983ac951b31878a2b22cc80f62bb0cb29db
ShunsukeYamamoto <59650988+ShunsukeYamamoto@users.noreply.github.com>
2020-01-29 16:48:56 +0900

    ビュー作成２

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :move_to_user_sign_in
+
   def index
     @user = current_user
+  end
+
+  private
+
+  def move_to_user_sign_in
+    redirect_to user_session_path unless user_signed_in?
   end
 end

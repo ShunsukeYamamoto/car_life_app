c3d3603ce35b6a9f3eea8996f71070ccc56360a3
ShunsukeYamamoto <59650988+ShunsukeYamamoto@users.noreply.github.com>
2020-03-12 16:29:52 +0900

    S3利用に伴う設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.12.1"
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.11.2'
 
 # Capistranoのログの表示に利用する
 set :application, 'car_life_app'
@@ -11,7 +12,7 @@ set :repo_url,  'git@github.com:ShunsukeYamamoto/car_life_app.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' 
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
@@ -24,10 +25,28 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
+#secrets.ymlではリリースバージョン間でシンボリックリンクにして共有
+#credentials.yml.encではmasterkeyにする（今回）
+set :linked_files, %w{config/master.key}
+
+
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
+
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
